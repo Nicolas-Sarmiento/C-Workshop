7781dd0c48ab62de50afba2dc44e84557e83fd28
Nicolas Tinjaca <136484809+NicolasT08@users.noreply.github.com>
2023-11-10 17:45:40 -0500

    Egoistical number function

---
+++ EgotisticalNumber.h
@@ -0,0 +1,10 @@
+//
+// Created by NickG on 10/11/2023.
+//
+
+#ifndef C_WORKSHOP_EGOTISTICALNUMBER_H
+#define C_WORKSHOP_EGOTISTICALNUMBER_H
+
+extern int isEgotistical(int number);
+
+#endif //C_WORKSHOP_EGOTISTICALNUMBER_H
