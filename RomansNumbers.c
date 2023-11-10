d4fd84c341a9d103ccfac23cb7ae585232b447c4
Nicolas-Sarmiento <nicolas.sarmiento@uptc.edu.co>
2023-11-10 17:53:22 -0500

    (Add) Roman numbers finished

--- RomansNumbers.c
+++ RomansNumbers.c
@@ -1,16 +1,24 @@
 //
-// Created by sarmi on 8/11/2023.
+// Created by Nicolas Sarmiento on 8/11/2023.
 //
 
 #include "RomansNumbers.h"
 #include <string.h>
-const char letterValues[] ={'I', 'V', 'X', 'L', 'C', 'D', 'M'};
-const int values[] = {1,5,10,50,100,500,1000 };
+#include <malloc.h>
+
+const int SIZE = 4;
+const  char* letterValues[] = { "I", "V", "X", "L", "C", "D", "M"};
+const  int values[] = {1,5,10,50,100,500,1000 };
 
 int* unitDecomposition( int number ) {
-    if ( number >= 10000 ) { return  0; }
+
+    if ( number > 4000 || number <= 0) { return  0; }
     int multiplies[] = {1000, 100, 10, 1};
-    static int units[] = {0,0,0,0};
+    int* units = (int *) malloc( SIZE * sizeof(int));
+    units[0] = 0;
+    units[1] = 0;
+    units[2] = 0;
+    units[3] = 0;
     int i = 0;
     do {
         if ( number / multiplies[i] == 0 ){
@@ -25,29 +33,57 @@ int* unitDecomposition( int number ) {
 
     return units;
 }
-char* calcDigitsInRoman( int number, int minValue, int maxValue ){
+
+char *calcDigitsInRoman( int number, int minValue, int maxValue ){
     if (number == 0 ){ return ""; }
+    char * result = (char*) malloc(10);
+    result[0] = '\0';
+    if ( number % 9 == 0 ){
+        strcat(result, letterValues[minValue]);
+        strcat(result, letterValues[maxValue + 1]);
+        return result;
+    }
 
-    char* digits[10];
-    if ( number % 9 == 0 ) {
-
-        strcat(digits, &letterValues[minValue] );
-        strcat(digits, &letterValues[maxValue+1] );
-        return digits;
+    if ( number == ( values[maxValue] - values[minValue])){
+        strcat(result, letterValues[minValue]);
+        strcat(result, letterValues[maxValue]);
+        return result;
     }
 
     do {
-        
+        if ( number >= values[maxValue] ){
+            number -= values[maxValue];
+            strcat(result, letterValues[maxValue]);
+            continue;
+        }
+
+        number -= values[minValue];
+        strcat( result, letterValues[minValue]);
+
     } while ( number );
-}
-char *romanNumber(int number) {
-    char* romanNumber;
-
-    return 0;
+    return result;
 }
 
+char *romanNumber( int number ) {
+    if ( number > 4000 || number <= 0 ){
+        return "Not exists";
+    }
 
+    char *result = (char *) malloc( 100);
+    result[0] = '\0';
+    int* units = unitDecomposition( number );
+    int minKeyValue = 6;
 
+    for (int i = 0; i < SIZE; i++) {
+        if ( i == 0) {
+            strcat(result, calcDigitsInRoman(units[i], minKeyValue -1, minKeyValue));
+            minKeyValue -=2;
+            continue;
+        }
 
-
-
+        strcat( result, calcDigitsInRoman( units[i], minKeyValue, minKeyValue +1 ));
+        minKeyValue -=2;
+    }
+    strcat(result, "\0");
+    return result;
+}
--- RomansNumbers.h
+++ RomansNumbers.h
@@ -6,6 +6,6 @@
 #define C_WORKSHOP_ROMANSNUMBERS_H
 
 int *unitDecomposition( int number );
+char *calcDigitsInRoman( int number, int minValue, int maxValue );
 char* romanNumber( int number );
-
 #endif //C_WORKSHOP_ROMANSNUMBERS_H
