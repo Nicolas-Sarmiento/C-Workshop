a6c71b7076e2f6a80034e0fa2d434722a83b7365
Nicolas Tinjaca <136484809+NicolasT08@users.noreply.github.com>
2023-11-09 21:15:04 -0500

    Prime numbers function

---
+++ PrimeNumbers.c
@@ -0,0 +1,27 @@
+//
+// Created by NickG on 9/11/2023.
+//
+
+#include "PrimeNumbers.h"
+#include <stdio.h>
+
+int primeNumbers(int n) {
+    int count;
+    printf("los factores primos de %d son: ", n);
+    for (int i = 2; i <= n; i++) {
+        if(count > 0){
+            printf(" * ");
+        }
+        count = 0;
+        while (n % i == 0) {
+            count+=1;
+            n /= i;
+            if(n % i != 0 && count > 1){
+                printf("%d^%d", i, count);
+            }
+            if(n % i != 0 && count == 1){
+                printf("%d", i);
+            }
+        }
+    }
+}
