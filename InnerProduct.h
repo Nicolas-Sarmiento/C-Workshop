a28945834212148645f3daac126e9f514ea4a2fb
Nicolas Tinjaca <136484809+NicolasT08@users.noreply.github.com>
2023-11-11 15:40:48 -0500

    Matrix Inner product

---
+++ InnerProduct.h
@@ -0,0 +1,10 @@
+//
+// Created by NickG on 11/11/2023.
+//
+
+#ifndef C_WORKSHOP_INNERPRODUCT_H
+#define C_WORKSHOP_INNERPRODUCT_H
+
+extern int innerProduct(int arrayOne[], int arrayTwo[], int size);
+
+#endif //C_WORKSHOP_INNERPRODUCT_H
