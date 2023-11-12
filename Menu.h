78f5c705278ba3fbdc2c13152383db347368a736
Nicolas Tinjaca <136484809+NicolasT08@users.noreply.github.com>
2023-11-11 21:26:35 -0500

    add methods to the .h file

--- Menu.h
+++ Menu.h
@@ -7,4 +7,7 @@
 extern void romanNumbersMenu();
 extern void amicableNumbersMenu();
 extern void magicSquareMenu();
+extern void primeNumbersMenu();
+extern void egoisticalNumberMenu();
+extern void innerProductMenu();
 #endif //C_WORKSHOP_MENU_H
